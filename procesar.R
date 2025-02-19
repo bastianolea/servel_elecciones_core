e824551f3835064efbb003a28bc11bdc8a6c2507
bastianolea <bastianolea@gmail.com>
2025-02-19 17:14:14 -0300

    corrección comunas

--- procesar.R
+++ procesar.R
@@ -2,6 +2,9 @@ library(janitor)
 library(readxl)
 library(readr)
 library(dplyr)
+library(stringr)
+
+source("funciones.R")
 
 # cargar ----
 concejales <- read_xlsx("datos/datos_originales/2024_10_Concejales_DatosEleccion.xlsx", 
@@ -21,31 +24,51 @@ cores <- read_xlsx("datos/datos_originales/2024_10_ConsejerosRegionales_DatosEle
 
 # calculado en clasificar_genero.R
 genero <- readr::read_rds("genero.rds")
-                             
+
+
+alcaldes |> 
+  distinct(region) |> 
+  mutate(region = stringr::str_to_title(region),
+         region = stringr::str_replace(region, " La ", " la"),
+         region = stringr::str_replace(region, " De ", " de "),
+         region = stringr::str_replace(region, " Del ", " del "),
+         region = stringr::str_replace(region, " Y ", " y "))
+
+
+# cargar comunas
+comunas <- read_rds("datos/cut_comuna.rds") |> 
+  mutate(comuna_b = tolower(nombre_comuna),
+         comuna_b = stringr::str_replace_all(comuna_b, 
+                                             c("á"="a", "é"="e", "í"="i", "ó"="o", "ú"="u",
+                                               "ü"="u"))
+  )
+
 
 
 # limpiar ----
-
 concejales_2 <- concejales |>
+  corregir_comunas() |> 
   # solo electos
   filter(!is.na(cargo)) |>
   left_join(genero, by = "nombres") |>
-  select(region, comuna, lista, pacto, nombres, primer_apellido, votos, genero)
+  select(nombre_region, codigo_region, nombre_comuna, codigo_comuna, lista, pacto, nombres, primer_apellido, votos, genero)
 
 alcaldes_2 <- alcaldes |>
+  corregir_comunas() |> 
   # solo electos
   filter(!is.na(cargo)) |>
   left_join(genero, by = "nombres") |>
-  select(region, comuna, lista, pacto, nombres, primer_apellido, votos, genero)
+  select(nombre_region, codigo_region, nombre_comuna, codigo_comuna, lista, pacto, nombres, primer_apellido, votos, genero)
 
 cores_2 <- cores |>
+  corregir_comunas() |> 
   # solo electos
   filter(!is.na(cargo)) |>
   # sumar por región
-  group_by(region, lista, pacto, nombres, primer_apellido) |> 
+  group_by(nombre_region, codigo_region, lista, pacto, nombres, primer_apellido) |> 
   summarize(votos = sum(votos)) |> 
   left_join(genero, by = "nombres") |>
-  select(region, lista, pacto, nombres, primer_apellido, votos, genero)
+  select(nombre_region, codigo_region, lista, pacto, nombres, primer_apellido, votos, genero)
 
 
 
@@ -53,3 +76,29 @@ cores_2 <- cores |>
 write_csv2(concejales_2, "datos/resultados_servel_2024_concejales.csv")
 write_csv2(alcaldes_2, "datos/resultados_servel_2024_alcaldes.csv")
 write_csv2(cores_2, "datos/resultados_servel_2024_cores.csv")
+
+
+
+# # candidaturas ----
+# 
+# concejales_b <- concejales |>
+#   # solo electos
+#   filter(!is.na(cargo)) |>
+#   left_join(genero, by = "nombres") |>
+#   select(region, comuna, lista, pacto, nombres, primer_apellido, votos, genero)
+# 
+# alcaldes_b <- alcaldes |>
+#   # solo electos
+#   filter(!is.na(cargo)) |>
+#   left_join(genero, by = "nombres") |>
+#   select(region, comuna, lista, pacto, nombres, primer_apellido, votos, genero)
+# 
+# cores_b <- cores |>
+#   # solo electos
+#   filter(!is.na(cargo)) |>
+#   # sumar por región
+#   group_by(region, lista, pacto, nombres, primer_apellido) |> 
+#   summarize(votos = sum(votos)) |> 
+#   left_join(genero, by = "nombres") |>
+#   select(region, lista, pacto, nombres, primer_apellido, votos, genero)
+# 
